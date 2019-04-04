164ece7483fc524372bb59c1371eb5d43487b4dc
grumply <sean@grump.ly>
2019-04-04 13:35:04 -0500

    Fix nix formula description. Update license.

--- default.nix
+++ default.nix
@@ -5,6 +5,6 @@ mkDerivation {
   src = ./.;
   libraryHaskellDepends = [ base pure pure-cond pure-prop ];
   homepage = "github.com/grumply/pure-visibility";
-  description = "Styled views";
+  description = "Element Visibility wrapper component";
   license = stdenv.lib.licenses.bsd3;
 }

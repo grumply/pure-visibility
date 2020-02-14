43b5ee50ff20b433ca3ee904471663397b6d9c51
Sean Hickman <sean@grumply.com>
2020-02-13 23:38:05 -0600

    Update for GHC/JS 8.6.5 and 2020

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 { mkDerivation, base, pure, pure-cond, pure-prop, stdenv }:
 mkDerivation {
   pname = "pure-visibility";
-  version = "0.7.0.0";
+  version = "0.8.0.0";
   src = ./.;
   libraryHaskellDepends = [ base pure pure-cond pure-prop ];
   homepage = "github.com/grumply/pure-visibility";
